6fa3b10809ccbabcb5836f1141d21389d9d2d8c9
Vishnu19091 <vishnusah368@gmail.com>
2025-10-29 09:42:05 +0530

    Updated Globals.css file and added additional components

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -6,9 +6,9 @@ export const metadata = {
 
 function Page() {
   return (
-    <div>
-      <h1>Account Page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Vishnu
+    </h2>
   );
 }
 
